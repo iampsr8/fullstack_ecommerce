972432d309d128f759d9017e4db0416293799ae2
Prateek Rath <prateekrath28@gmail.com>
2023-06-14 13:12:12 +0530

    added products component

--- src/data.js
+++ src/data.js
@@ -39,3 +39,38 @@ export const categories = [
     title: "LIGHT JACKETS",
   },
 ];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id:2,
+    img:"https://ap.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton--varsity-blouson--HOL07EU35900_PM2_Front%20view.jpg",
+  },
+  {
+    id:3,
+    img:"https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-cluny-mm--M46374_PM2_Front%20view.jpg",
+  },
+  {
+    id:4,
+    img:"https://images.vestiairecollective.com/cdn-cgi/image/w=3840,q=70,f=auto,/produit/multicolour-cotton-prada-shirt-19702199-1_2.jpg",
+  },
+  {
+    id:5,
+    img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id:6,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id:7,
+    img:"https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/7815ef11-1c6f-482e-92cc-012c26d38271/air-jordan-1-mid-shoes-86f1ZW.png",
+  },
+  {
+    id:8,
+    img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+]

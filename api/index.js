9e325c5f001199482c10dc90f71aa88abf5e633a
Prateek Rath <prateekrath28@gmail.com>
2023-06-16 02:02:47 +0530

    create REST API

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB connection successful")).
+    catch((err) => console.log(err));
+
+
+// app.get("/api/test", () => {
+//     console.log("Test successful")
+// })
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(5000, () => {
+    console.log("server running on port 5000")
+})

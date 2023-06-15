82651d4b99da7c2e400db700dee7b10f2822b1a6
Prateek Rath <prateekrath28@gmail.com>
2023-06-16 02:05:23 +0530

    create api express routes

---
+++ api/routes/user.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+
+
+
+module.exports = router
+
+
+
+// router.get("/usertest", (req, res) => {
+//     res.send("user test is successful")
+// });
+
+// router.post("/userposttest", (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     res.send("your username is " + username)
+// });

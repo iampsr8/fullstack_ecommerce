db9fc4ca5afe8a4cc11aed7abb6306497e4ab558
Prateek Rath <prateekrath28@gmail.com>
2023-06-17 01:41:30 +0530

    implement get user stats by admin

--- api/routes/user.js
+++ api/routes/user.js
@@ -54,7 +54,31 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 })
 
 
-
+//get user stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    //lets say the admin wants the user data for the number of users that joined last year in different months
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
+    
+    try {
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" }
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total:{ $sum: 1 }
+            }}
+        ]);
+        return res.status(200).json(data);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
 
 module.exports = router

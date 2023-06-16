da1c92393e4a5574dd6ee17ffb1d5b7d818e19ab
Prateek Rath <prateekrath28@gmail.com>
2023-06-17 00:08:32 +0530

    jwt user authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User")
-const CryptoJS=require("crypto-js")
+const CryptoJS = require("crypto-js")
+const jwt=require("jsonwebtoken")
 
 //Register
 router.post("/register", async (req, res) => {
@@ -34,18 +35,28 @@ router.post("/login", async (req, res) => {
         //finding password from the user json object in the db
         const hashedPassword=CryptoJS.AES.decrypt(user.password,process.env.PASS_KEY)
         const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
         
         //return error is password from db and entered password does not match
         if (Originalpassword !== req.body.password) {
             return res.status(401).json("Wrong Password")
         }
 
+
+        //jwt user authentication
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_KEY,
+            {expiresIn:"3d"}
+        );
+
         //if everything is find return the user with status code 200 all OK
 
         //i don't want to send the password to the user only the other information, i'm gonna use the spread
         //operator to separate the password and the other information
         const { password, ...others } = user._doc;  //user info is stored inside _doc in mongodb
-        return res.status(200).json(others);
+        return res.status(200).json({...others,accessToken});
 
     } catch (err) {
         res.status(500).json(err);
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,24 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const CryptoJS = require("crypto-js")
+
 const router = require("express").Router();
 
 
+//update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password= CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString()
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set:req.body
+        }, { new: true })
+        return res.status(200).json(updatedUser)
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 
 
 module.exports = router
